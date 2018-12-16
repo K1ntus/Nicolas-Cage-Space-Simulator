b702b7a32a0445d6f708e816ed4c5156cdd83653
K1ntus <masson.jordan@gmail.com>
2018-12-17 00:08:09 +0100

    comments

--- src/fr/groupe40/projet/util/constants/GenerationErrorSide.java
+++ src/fr/groupe40/projet/util/constants/GenerationErrorSide.java
@@ -1,28 +1,35 @@
 package fr.groupe40.projet.util.constants;
 
-
+/**
+ * Used for planet generation
+ * Return a specific value if his position make 
+ * the planet out of bounds
+ *
+ */
 public enum GenerationErrorSide {
 	
 	/**
-	 * There is no collisions
-	 *
+	 * Planet is oob (out of bounds) to the left side
 	 */
 	GREATER_X,
-	
+
 	/**
-	 * TOP collision between a ship and a planet
+	 * Planet is oob (out of bounds) to the right side
 	 */
 	LOWER_X,
-	
+
 	/**
-	 * BOTTOM collision between a ship and a planet
+	 * Planet is oob (out of bounds) to the bottom side
 	 */
 	GREATER_Y,
-	
+
 	/**
-	 * LEFT collision between a ship and a planet
+	 * Planet is oob (out of bounds) to the top side
 	 */
 	LOWER_Y,
 	
+	/**
+	 * If the current osition fit the board
+	 */
 	NO_PROBLEM
 }
