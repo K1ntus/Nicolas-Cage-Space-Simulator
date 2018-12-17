4c262fb15b4ff5cf66ddbd904db7da09f32aab68
Inopio <email random>
2018-12-17 18:22:47 +0100

    c tout KC

--- src/fr/groupe40/projet/junit/ClasseTests.java
+++ src/fr/groupe40/projet/junit/ClasseTests.java
@@ -1,6 +1,4 @@
 package fr.groupe40.projet.junit;
-import static org.junit.Assert.assertEquals;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,8 +9,9 @@ import fr.groupe40.projet.model.planets.SquarePlanet;
 import fr.groupe40.projet.model.ships.Ship;
 import fr.groupe40.projet.util.constants.Generation;
 import fr.groupe40.projet.util.constants.ShipsParameters;
+import junit.framework.TestCase;
 
-public class ClasseTests {
+public class ClasseTests extends TestCase{
 	/**
 	 * test of the function setX() with case 0.0
 	 */
@@ -20,6 +19,7 @@ public class ClasseTests {
 	 public void testSetX() {
 		 Ship tester = new Ship(null, null, null, null, 3, 10, null); // MyClass is tested
 		 tester.setX(0.0);
+		
 		 assert 0.0 == tester.getX();
 		 //assertEquals(0.0, tester.getX(), 0);
 	}
@@ -31,6 +31,7 @@ public class ClasseTests {
 	public void testSetY() {
 		 Ship tester = new Ship(null, null, null, null, 10, 40, null); // MyClass is tested
 		 tester.setY(5.0);
+		 
 		 assert 5.0 == tester.getY();
 	    // assertEquals(5.0, tester.getY());
 	}
@@ -42,6 +43,7 @@ public class ClasseTests {
 	public void testSetMinX() {
 		 Ship tester = new Ship(null, null, null, null, 13, 14, null); // MyClass is tested
 		 tester.setMinX(0.0);
+		
 		 assert 0.0 == tester.getMinX();
 	     //assertEquals(0.0, tester.getMinX());
 	}
@@ -53,6 +55,7 @@ public class ClasseTests {
 	public void testSetMinY() {
 		 Ship tester = new Ship(null, null, null, null, 11, 12, null); // MyClass is tested
 		 tester.setMinY(1.0);
+		
 		 assert 1.0 == tester.getMinY();
 	    // assertEquals(1.0, tester.getMinY());
 	}
@@ -64,6 +67,7 @@ public class ClasseTests {
 	public void testisInsideSquarePlanet() {
 		 Ship tester = new Ship(null, null, null, null, 11, 12, null); // MyClass is tested
 		 SquarePlanet p = new SquarePlanet(null, null, 10, 10);
+		 
 		 assert true ==  tester.isInside(p);
 	     //assertEquals(true, tester.isInside(p));
 	}
@@ -77,29 +81,31 @@ public class ClasseTests {
 		 Ship tester = new Ship(null, null, null, null, 11, 12, null); // MyClass is tested
 		 SquarePlanet p = new SquarePlanet(null, null, 900, 900);
 		 assertEquals(false, tester.isInside(p));
+		
 		 assert false ==  tester.isInside(p);
 	     //assertEquals(true, tester.isInside(p));
 	}
 	
+	/*
 	
-	/**
-	 * test of the function isInside(Sprite s) with RoundPlanet objects, case false
-	 */
+	 //test of the function isInside(Sprite s) with RoundPlanet objects, case false
+	 
 	@Test
 	public void testisInsideRoundPlanet() {
 		 Ship tester = new Ship(null, null, null, null, 11, 12, null); // MyClass is tested
 		 SquarePlanet p = new SquarePlanet(null, null, 900, 900);
 		 assert false ==  tester.isInside(p);
 	}
-	
+	*/
 
 	/**
-	 * test of the function isInside(Sprite s) with RoundPlanet objects, case true
+	 * test of the function isInside(Sprite s) with SquarePlanet objects, case true
 	 */
 	@Test
 	public void testisInsideRoundPlanet2() {
 		 Ship tester = new Ship(null, null, null, null, 10, 10, null); // MyClass is tested
 		 SquarePlanet p = new SquarePlanet(null, null, 10, 10);
+		 
 		 assert true ==  tester.isInside(p);
 	}
 	
@@ -110,6 +116,7 @@ public class ClasseTests {
 	@Test
 	public void testisInside2() {
 		 Ship tester = new Ship(null, null, null, null, 11, 12, null);
+		 
 		 assert false == tester.isInside(500,500, Generation.size_minimal_planets, Generation.size_minimal_planets);
 		
 	}
@@ -134,6 +141,7 @@ public class ClasseTests {
 		double a = 100.0;
 		double b = 200.0;
 		double c = Math.sqrt((a * a) + (b * b));
+		
 		assert c == tester.distance(tester.getX(), tester.getY(),a,b);
 	}
 	
@@ -148,6 +156,7 @@ public class ClasseTests {
 		double x = 100.0 - (Generation.size_squads /2);
 		double y = 200.0 - (Generation.size_squads /2);
 		double c = Math.sqrt((x * x) + (y * y));
+		
 		assert c == tester.distance(p);
 		
 	}
@@ -162,12 +171,13 @@ public class ClasseTests {
 		double x = 100.0 - (Generation.size_squads /2);
 		double y = 200.0 - (Generation.size_squads /2);
 		double c = Math.sqrt((x * x) + (y * y));
+		
 		assert c == tester.distance(p);
 		
 	}
 	
 	/**
-	 * test of the function validatePosition() with a RoundPlanet at a pos of (-10,-10)
+	 * test of the function validatePosition() with a SquarePlanet at a pos of (-10,-10)
 	 */
 	/*
 	@Test
@@ -184,7 +194,7 @@ public class ClasseTests {
 	*/
 	
 	/**
-	 * test of the function whereis_collision(double x, double y, double speed, List<Planet> planets) with a RoundPlanet at a pos of (150,150)
+	 * test of the function whereis_collision(double x, double y, double speed, List<Planet> planets) with a SquarePlanet at a pos of (150,150)
 	 * \and a ship
 	 */
 	@Test
@@ -199,7 +209,7 @@ public class ClasseTests {
 	}
 	
 	/**
-	 * test of the function calc_next_position with a RoundPlanet at a pos of (150,150)
+	 * test of the function calc_next_position with a SquarePlanet at a pos of (150,150)
 	 * \and a ship
 	 */
 	@Test
@@ -209,13 +219,16 @@ public class ClasseTests {
 		List<Planet> planets = new ArrayList<Planet>();
 		planets.add(p);
 		
+		double x = tester.getX();
+		double y = tester.getY();
 		tester.setDestination(p);
 		
 		tester.calc_next_position(planets);
+		assert x != tester.getX() && y != tester.getY();
 		
 	}
 	/**
-	 * \brief test of the function Top_collision_mover with a RoundPlanet at a pos of (100,200)
+	 * \brief test of the function Top_collision_mover with a SquarePlanet at a pos of (100,200)
 	 * \and a ship
 	 */
 	@Test
@@ -225,9 +238,25 @@ public class ClasseTests {
 		double x = tester.getX();
 		double y = tester.getY();
 		tester.top_collision_mover(p.getX(), p.getY(),  p.getX() + p.width()/2,  p.getY() + p.height()/2, tester.getSpeed());
+		
 		assert x != tester.getX();
-		assert y == tester.getY();
+		assert y != tester.getY();
 	}
 
-	
+	/**
+	 * \brief test of the function botton_collision_mover with a SquarePlanet at a pos of (150,100)
+	 * \and a ship
+	 */
+	@Test
+	public void Testbottom_collision_mover() {
+		Ship tester = new Ship(null, null, null, null, 0, 0, null);
+		SquarePlanet p = new SquarePlanet(null, null, 100, 200);
+		double x = tester.getX();
+		double y = tester.getY();
+		tester.bottom_collision_mover(p.getX(), p.getY(),  p.getX() + p.width()/2,  p.getY() + p.height()/2, tester.getSpeed());
+		
+		assert x != tester.getX();
+		assert y != tester.getY();
+	}
+
 }
